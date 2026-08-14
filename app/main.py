df19957636a9d670930d4ae3990064e8e4c24df4
Ajero Franklin <244808567+Franklindot04@users.noreply.github.com>
2026-08-14 04:12:13 +0000

    Initialize SQLite database on app startup
    Co-authored-by: Franklin <244808567+Franklindot04@users.noreply.github.com>
    Co-authored-by: Ellesmaris <280987152+ellesmaris@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.api.incidents import router as incidents_router
+from app.db.sqlite import init_db
+
 
 app = FastAPI()
+init_db()
 
 app.include_router(incidents_router, prefix="/incidents")
 
